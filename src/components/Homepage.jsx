cc673f23c38280390ca563bf50348c653d2e8fe4
_Allan <allankipchumba76@gmail.com>
2022-10-29 18:50:11 +0300

    installed sass and tailwind

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
+import styles from "./homepage/homepage.module.scss";
 
 export const Homepage = () => {
-  return <div>Homepage</div>;
+  return <div className={styles.homepage}>Homepage</div>;
 };
