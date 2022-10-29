8bcf7c4e663b2dac5a1ae3c9a762f8143a03e48a
_Allan <allankipchumba76@gmail.com>
2022-10-29 18:22:05 +0300

    initiated project

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default (props) => {
-  return (
-    <>
-      <p>Nothing too much here, so go ahead and get coding!</p>
-    </>
-  );
-};
---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Homepage } from "./components/Homepage";
+
+export const App = () => {
+  return <Homepage />;
+};
